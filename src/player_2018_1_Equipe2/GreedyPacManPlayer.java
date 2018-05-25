dcaf21f39a74b02679c64cb47ac4ce59f2b88497
Maycow <maycow.carmo@gmail.com>
2018-05-25 16:47:55 -0300

    comecei
    travado

---
+++ src/player_2018_1_Equipe2/GreedyPacManPlayer.java
@@ -0,0 +1,13 @@
+package player_2018_1_Equipe2;
+
+import pacman.Game;
+import pacman.Move;
+import player.DFSPacManPlayer;
+
+public class GreedyPacManPlayer extends DFSPacManPlayer {
+
+	public Move chooseMove(Game game) {
+		return null;
+		
+	}
+}
