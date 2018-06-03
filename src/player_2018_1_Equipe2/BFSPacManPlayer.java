7bab27f067dfa9e02b3910c3938dd014904410ac
Maycow <maycow.carmo@gmail.com>
2018-06-03 20:34:07 -0300

    heuristicas novas
    beep

--- src/player_2018_1_Equipe2/BFSPacManPlayer.java
+++ src/player_2018_1_Equipe2/BFSPacManPlayer.java
@@ -16,13 +16,14 @@ public class BFSPacManPlayer extends DFSPacManPlayer{
 	//private Move lastMove = null; //vai ser usado para evitar o retorno
 	private Move bestMove = null;
 	private Random random = new Random();
+	private int lives;
 
 	private ArrayList<Move> lastMoves = new ArrayList<Move>();
 
 
 	@Override
 	public Move chooseMove(Game game) {
-		
+		lives = game.getLives();
 		this.bestMove = breadthFirstSearch(game.getCurrentState(), game);
 		return bestMove;
 	}
@@ -30,14 +31,22 @@ public class BFSPacManPlayer extends DFSPacManPlayer{
 	//HEURISTICAS
 	@Override
 	public double evaluateState(State state) {
-		//Verifica se o proximo estado vai fazer o pacman perder
-		if (Game.isFinal(state))
+		double score = 0.0;
+		int riscMultiplier;
+
+		//Verifica se o proximo estado perderia o jogo
+		if (Game.isLosing(state))
 			return -1000.0;
 		//Verifica se no proximo estado ganharia o jogo
 		if (Game.isWinning(state))
-			return 10.0;
+			return 0.0;
 
-		double score = 0.0;
+
+		if(lives > 1){
+			riscMultiplier = 2;
+		}else{
+			riscMultiplier = 1;
+		}
 
 		//loc do pacman no proximo state
 		Location pacManLoc = state.getPacManLocation();
@@ -51,13 +60,18 @@ public class BFSPacManPlayer extends DFSPacManPlayer{
 
 		Location closestGhost = getClosest(pacManLoc, state.getGhostLocations());
 
+		//preferencia para pegar pontos
+		if(state.getDotLocations().size() < oldState.getDotLocations().size()){
+			score += 500 * riscMultiplier;
+		}
+
 		//distancia media do fantasma mais perto do pacman para os outros fantasmas
 		score -= getMedDistance(closestGhost, state.getGhostLocations());
 		//System.out.println("medDistance " + getMedDistance(closestGhost, state.getGhostLocations()));
 		//System.out.println("score: " + score);
 
 		//Distancia do pacman para o fantasma mais perto
-		score += Location.manhattanDistanceToClosest(pacManLoc, state.getGhostLocations()) *1.5;
+		score += Location.manhattanDistanceToClosest(pacManLoc, state.getGhostLocations()) * 1.23;
 
 		//distancia do pacman para o ponto mais perto
 		score -= Location.manhattanDistance(pacManLoc, closestDot);
